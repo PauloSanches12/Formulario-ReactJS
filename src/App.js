5e62acffd538e7ac575332384970c83c17cdfd29
Paulo <paulo_sanches2012@hotmail.com>
2020-03-10 01:19:47 -0300

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react';
+import { Form } from '@unform/web';
 import './App.css';
+import Input from './components/Form/Input';
+import { Scope } from '@unform/core';
+import * as Yup from 'yup';
 
 function App() {
+  const FormRef = useRef(null);
+
+  async function handleSubmit(data, { reset }){
+    try {
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('O e-mail é obrigatório'),
+        address: Yup.object().shape({
+          city: Yup.string()
+            .min(3, 'no mínimo 3 caracteres')
+            .required('A cidade é obrigatória')
+        })
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+
+      console.log(data);
+
+      FormRef.current.setErrors({});
+
+      reset();
+    } catch (err){
+      if (err instanceof Yup.ValidationError){
+        const errorMessages = {};
+
+        err.inner.forEach(error => {
+          errorMessages[error.path] = error.message;
+        })
+
+        FormRef.current.setErrors(errorMessages);
+      }
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      FormRef.current.setData({
+        name: 'Paulo Sérgio',
+        email: 'paulo_sanches2012@hotmail.com',
+        address: {
+          city: 'Palmas'
+        }
+
+      })
+    }, 2000)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form ref={FormRef} onSubmit={handleSubmit}>
+      <img
+        src="https://storage.googleapis.com/golden-wind/unform/unform.svg"
+        height="150"
+        width="175"
+        alt="Unform"
+      />
+        <Input label="Nome Completo" name="name" />
+        <Input label="E-mail" name="email" />
+
+        <Scope path="address" >
+          <Input name="street" label="Rua" />
+          <Input name="neighborhood" label="Bairro" />
+          <Input name="city" label="Cidade" />
+          <Input name="state" label="Estado" />
+          <Input name="number" label="CEP" />
+        </Scope>
+
+        <button type="submit">Enviar</button>
+      </Form>
     </div>
   );
 }
